c78f7753f66764b1915db20b7fcb6889ccbedd30
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-15 17:14:53 +0100

    correzione mail

--- mail/main.js
+++ mail/main.js
@@ -1,19 +1,25 @@
-let mail = ["pippo@gmail.com", "gigio@gmail.com", "franco@gmail.com", "bruno@gmail.com"];
+let mail = ["pippo@gmail.com", 
+            "gigio@gmail.com", 
+            "franco@gmail.com", 
+            "bruno@gmail.com"];
 totMail = mail.length;
 let chiediMail= prompt("dimmi la tua mail");
 console.log(mail)
 console.log(chiediMail)
 
-
+let mailDaTrovare = false
 
 for(let i=0 ; i <= totMail; i++ ){
     console.log(mail)
     if(mail[i]==chiediMail){
-        document.getElementById("cont").innerHTML += "<div>sei iscitto</div>"
-    }
-    else{
-        document.getElementById("cont").innerHTML += "<div>non sei iscitto</div>"
+        mailDaTrovare=true
     }
 } 
 
 
+if(mailDaTrovare==true){
+        document.getElementById("cont").innerHTML += "<div>sei iscritto</div>"
+    }
+else{
+        document.getElementById("cont").innerHTML += "<div>non sei iscitto</div>"
+    }
